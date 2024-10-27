e85bf6ba307269df5a037e65471f8ce8d28cf767
Whiterabitneo <daily.wise.inspiration@gmail.com>
2024-10-27 06:55:34 -0700

    server.js

--- server.js
+++ server.js
@@ -38,5 +38,5 @@ app.post('/sendEmail', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(Server is running on http://localhost:${PORT});
+    console.log('Server is running on http://localhost:${PORT}');
 });
