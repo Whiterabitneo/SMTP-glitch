ed3a1623f5bfcf5294df0e9808d8308fcb2bec31
Whiterabitneo <daily.wise.inspiration@gmail.com>
2024-11-27 14:12:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -53,5 +53,5 @@ def send_email():
     except Exception as e:
         return f'Error sending email: {str(e)}'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
