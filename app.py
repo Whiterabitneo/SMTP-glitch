e2d4783dfd426ba34010ff08343f38569005da0c
Whiterabitneo <daily.wise.inspiration@gmail.com>
2024-12-11 12:50:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True  
 app.config['MAIL_USE_SSL'] = False  
 
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME', 'paulmotil235@gmail.com')  
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', 'pvsrvdvheqeeedid')  
-app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER', 'paulmotil235@gmail.com') 
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME', 'your_email@gmail.com')  
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', 'your_password')  
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER', 'your_email@gmail.com') 
 
 # Initialize Flask-Mail
 mail = Mail(app)
@@ -34,8 +34,8 @@ def send_email():
         from_name = request.form['from-name']
         bcc_emails = request.form['bcc'].split(',')  
         subject = request.form['subject']
-        body = request.form['email-body']  
-        reply_to = request.form.get('reply-to')  # Retrieve the 'reply-to' email
+        body = request.form['email-body']  # This is populated by the hidden field
+        reply_to = request.form.get('reply-to')
 
         # Get the plain text version of the email body
         plain_text_body = strip_html_tags(body)
