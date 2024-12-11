bc0a6fd5e2bd34446efd5f898b8b3306507433f5
Whiterabitneo <daily.wise.inspiration@gmail.com>
2024-12-11 13:06:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True  
 app.config['MAIL_USE_SSL'] = False  
 
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME', 'your_email@gmail.com')  
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', 'your_app_password')  
-app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER', 'your_email@gmail.com') 
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME', 'paulmotil235@gmail.com')  
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', 'pvsrvdvheqeeedid')  
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER', 'paulmotil235@gmail.com') 
 
 # Initialize Flask-Mail
 mail = Mail(app)
